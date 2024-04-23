e2cab62fe1aa6eb2c4fd2651adca0217f73e5098
phucvin <phucvin@gmail.com>
2024-04-23 19:09:49 +0000

    read another file error

--- examples/rust/src/main.rs
+++ examples/rust/src/main.rs
@@ -6,4 +6,8 @@ fn main() {
         Ok(contents) => println!("'a' contents:\n{contents}"),
         Err(e) => println!("reading 'a' error: {e}"),
     };
+    match fs::read_to_string("b") {
+        Ok(contents) => println!("'b' contents:\n{contents}"),
+        Err(e) => println!("reading 'b' error: {e}"),
+    };
 }
